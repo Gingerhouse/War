2db4b5f86a198c32dcdcc2a2a86446ef16a1b525
Gingerhouse <abelvillape@gmail.com>
2022-05-31 15:05:45 -0400

    Started Networking for Game

---
+++ client.py
@@ -0,0 +1,25 @@
+import pygame
+from player import Player
+
+width = 500
+height = 500
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Client')
+
+clientNumber = 0
+
+def redrawWindow():
+    win.fill((255, 255, 255))
+    pygame.display.update()
+
+def main():
+    run = True
+
+    p = Player('name', 'nation')
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+
