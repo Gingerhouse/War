2d11123346cb31f8d205607569774c73cc299331
Gingerhouse <abelvillape@gmail.com>
2022-06-03 14:00:11 -0400

    Updated Player

--- player.py
+++ player.py
@@ -1,8 +1,8 @@
 class Player:
-    def __init__(self, name, nation):
+    def __init__(self, name, nation, pos):
         self.name = name
         self.nation = nation
-        self.pos
+        self.pos = pos
         self.points = 0
         self.action = []
         self.requests = []
