d9f49d3745bd9a7d6672ea910487590b7ad8c4da
Gingerhouse <abelvillape@gmail.com>
2022-06-18 11:29:33 -0400

    Starting to Merge

--- client.py
+++ client.py
@@ -13,25 +13,12 @@ def redrawWindow():
 
 
 def main():
-    run = True
-    n = Network()
-    p = n.getP()
+    pass
 
-    clock = pygame.time.Clock()
+def instructions():
+    pass
 
-    while run:
-        clock.tick(60)
-        p2 = n.send(p)
-        p3 = n.send(p)
-        p4 = n.send(p)
-        p5 = n.send(p)
-        p6 = n.send(p)
-        p7 = n.send(p)
-        p8 = n.send(p)
+def game():
+    pass
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
-
-        redrawWindow(win, p, p2, p3, p4, p5, p6, p7, p8)
+main()
